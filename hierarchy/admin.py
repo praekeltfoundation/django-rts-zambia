aec783adfc4f2433f54f70ac10e8dd36d40fa02f
Mark Gituma <gitumarkk@gmail.com>
2013-08-05 17:39:31 +0200

    registered models with Admin

---
+++ hierarchy/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import (Provinces, Districts, Zones, Schools)
+
+
+admin.site.register(Provinces)
+admin.site.register(Districts)
+admin.site.register(Zones)
+admin.site.register(Schools)
