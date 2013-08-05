e04295f92bd7fc2e13b5dfc3e245bc135780486e
Mark Gituma <gitumarkk@gmail.com>
2013-08-05 16:38:54 +0200

    creating the models

--- hierarchy/models.py
+++ hierarchy/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Provinces(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Name of Province')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Province"
+
+
+class Districts(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Name of District')
+    province_id = models.ForeignKey('MonitorAndLearningQuizId',
+                                    related_name='province_id ')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Monitor and Learning Quiz Question"
+
+
+
+class Zones(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Name of Zone')
+    district_id = models.ForeignKey('MonitorAndLearningQuizId',
+                                    related_name='district_id ')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Zone"
+
+
+class Schools(models.Model):
+    EMIS = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name=u'Name of School')
+    school_id = models.ForeignKey('MonitorAndLearningQuizId',
+                                   related_name='school_id ')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "School"
