2292db1eb1ea2d5afa3de9f1537babcb8d5e8d20
Mark Gituma <gitumarkk@gmail.com>
2013-08-05 18:59:01 +0200

    Coded th eAPI

---
+++ hierarchy/api.py
@@ -0,0 +1,79 @@
+from tastypie.resources import ModelResource, ALL_WITH_RELATIONS
+from django.conf.urls import url
+from tastypie import fields
+from models import (Provinces, Districts, Zones, Schools)
+
+
+class ProvinceResource(ModelResource):
+    """
+    This class:
+        - Adds resource_name for the API
+        - Returns the required data for the API via Foreign key association,
+        based on the url
+    """
+    # path = 'monitor_and_learn.api.MonitorAndLearningQuizQuestionResource'
+    # quiz_ids = fields.ToManyField(path,
+    #                               'quiz_ids', full=True)
+
+    class Meta:
+        resource_name = "hierarchy"
+        allowed_methods = ['get']
+        include_resource_uri = False
+        queryset = Provinces.objects.all()
+
+
+class DistrictResource(ModelResource):
+    """
+    This class:
+        - Adds resource_name for the API
+        - Returns the required data for the API via Foreign key association,
+        based on the url
+    """
+    # path = 'monitor_and_learn.api.MonitorAndLearningQuizQuestionResource'
+    # quiz_ids = fields.ToManyField(path,
+    #                               'quiz_ids', full=True)
+    province_id = fields.ForeignKey(ProvinceResource, 'province_id', full=True)
+    class Meta:
+        resource_name = "hierarchy"
+        allowed_methods = ['get']
+        include_resource_uri = False
+        queryset = Districts.objects.all()
+
+
+class ZoneResource(ModelResource):
+    """
+    This class:
+        - Adds resource_name for the API
+        - Returns the required data for the API via Foreign key association,
+        based on the url
+    """
+    # path = 'monitor_and_learn.api.MonitorAndLearningQuizQuestionResource'
+    # quiz_ids = fields.ToManyField(path,
+    #                               'quiz_ids',
+    #                               full=True)
+    district_id = fields.ForeignKey(DistrictResource, 'district_id', full=True)
+    class Meta:
+        resource_name = "hierarchy"
+        allowed_methods = ['get']
+        include_resource_uri = False
+        queryset = Zones.objects.all()
+
+
+class SchoolResource(ModelResource):
+    """
+    This class:
+        - Adds resource_name for the API
+        - Returns the required data for the API via Foreign key association,
+        based on the url
+    """
+    # path = 'monitor_and_learn.api.MonitorAndLearningQuizQuestionResource'
+    # school_id = fields.ToManyField(path,
+    #                               'school_id',
+    #                               full=True)
+    
+    zone_id = fields.ForeignKey(ZoneResource, 'zone_id', full=True)
+    class Meta:
+        resource_name = "hierarchy"
+        allowed_methods = ['get']
+        include_resource_uri = False
+        queryset = Schools.objects.all()
