e7f36c2b9c20202d26d322602e6b8955f6f3d289
Mike Jones <mike@westerncapelabs.com>
2013-08-05 23:05:12 +0200

    completed reg questions

--- js_sandbox/test/test-go-rts-zambia.js
+++ js_sandbox/test/test-go-rts-zambia.js
@@ -353,5 +353,124 @@ describe("When using the USSD line as an unrecognised MSISDN", function() {
         p.then(done, done);
     });
 
+    it("entering student girls G2 number should ask for zonal head", function (done) {
+        var user = {
+            current_state: 'reg_school_students_g2_girls',
+            answers: {
+                initial_state: 'reg_emis',
+                reg_emis: '0001',
+                reg_school_name: 'School One',
+                reg_first_name: 'Jack',
+                reg_surname: 'Black',
+                reg_date_of_birth: '11-Sep-1980',
+                reg_gender: 'male',
+                reg_school_classrooms: '5',
+                reg_school_teachers: '5',
+                reg_school_teachers_g1: '2',
+                reg_school_teachers_g2: '2',
+                reg_school_students_g2_boys: '10'
+            }
+        };
+        var p = tester.check_state({
+            user: user,
+            content: "11",
+            next_state: "reg_zonal_head",
+            response: "^Are you a Zonal Head\\?[^]" +
+                    "1. Yes[^]" +
+                    "2. No$"
+        });
+        p.then(done, done);
+    });
+
+    it("saying not zonal head should ask for zonal head name", function (done) {
+        var user = {
+            current_state: 'reg_zonal_head',
+            answers: {
+                initial_state: 'reg_emis',
+                reg_emis: '0001',
+                reg_school_name: 'School One',
+                reg_first_name: 'Jack',
+                reg_surname: 'Black',
+                reg_date_of_birth: '11-Sep-1980',
+                reg_gender: 'male',
+                reg_school_classrooms: '5',
+                reg_school_teachers: '5',
+                reg_school_teachers_g1: '2',
+                reg_school_teachers_g2: '2',
+                reg_school_students_g2_boys: '10',
+                reg_school_students_g2_girls: '11'
+            }
+        };
+        var p = tester.check_state({
+            user: user,
+            content: "2",
+            next_state: "reg_zonal_head_name",
+            response: "^What is the name and surname of your Zonal Head\\?$"
+        });
+        p.then(done, done);
+    });
+
+    it("saying are zonal head should thank long and close", function (done) {
+        var user = {
+            current_state: 'reg_zonal_head',
+            answers: {
+                initial_state: 'reg_emis',
+                reg_emis: '0001',
+                reg_school_name: 'School One',
+                reg_first_name: 'Jack',
+                reg_surname: 'Black',
+                reg_date_of_birth: '11-Sep-1980',
+                reg_gender: 'male',
+                reg_school_classrooms: '5',
+                reg_school_teachers: '5',
+                reg_school_teachers_g1: '2',
+                reg_school_teachers_g2: '2',
+                reg_school_students_g2_boys: '10',
+                reg_school_students_g2_girls: '11'
+            }
+        };
+        var p = tester.check_state({
+            user: user,
+            content: "1",
+            next_state: "reg_thanks_zonal_head",
+            response: "^Thank you for registering! When you are ready you can " +
+            "dial in again to start reporting. You will also start receiving the " +
+            "monthly SMS's from your Headteachers.$",
+            continue_session: false
+        });
+        p.then(done, done);
+    });
+
+    it("entering name of zonal head should thank and close", function (done) {
+        var user = {
+            current_state: 'reg_zonal_head_name',
+            answers: {
+                initial_state: 'reg_emis',
+                reg_emis: '0001',
+                reg_school_name: 'School One',
+                reg_first_name: 'Jack',
+                reg_surname: 'Black',
+                reg_date_of_birth: '11-Sep-1980',
+                reg_gender: 'male',
+                reg_school_classrooms: '5',
+                reg_school_teachers: '5',
+                reg_school_teachers_g1: '2',
+                reg_school_teachers_g2: '2',
+                reg_school_students_g2_boys: '10',
+                reg_school_students_g2_girls: '11',
+                reg_zonal_head: 'reg_zonal_head_name'
+            }
+        };
+        var p = tester.check_state({
+            user: user,
+            content: "Jim Carey",
+            next_state: "reg_thanks_head_teacher",
+            response: "^Thank you for registering! When you are ready you can dial " +
+            "in again to start reporting.$",
+            continue_session: false
+        });
+        p.then(done, done);
+    });
+
 });
 
