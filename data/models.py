174d94cc4020bd81b4f0f9157e4fb58a9a259b35
Mark Gituma <gitumarkk@gmail.com>
2013-08-06 18:34:24 +0200

    created basic models for data

--- data/models.py
+++ data/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from hierarchy.models import School
 
-# Create your models here.
+
+class SchoolData(models.Model):
+    EMIS = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, verbose_name=u'Name of School')
+    zone_id = models.ForeignKey('Zones',
+                                   related_name='zone_id ')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "School"
+
+
+class HeadTeacher(models.Model):
+    name_first = models.CharField(max_length=50, verbose_name=u'First Name')
+    name_last = models.CharField(max_length=50, verbose_name=u'Last Name')
+    gender = models.CharField(max_length=2, verbose_name=u'Gender',
+                              choices =(("M", "Male"), ("F", "Female")))
+    date_of_birth = models.DateField()
+    is_zonal_head = models.BooleanField()
+    EMIS_id = models.IntegerField(School,
+                                  related_name="EMIS")
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Province"
