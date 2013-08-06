39817671f3f5f534abcbc79fe604e983960ea59f
Mark Gituma <gitumarkk@gmail.com>
2013-08-06 22:04:35 +0200

    updated models.py to include external foreign key

--- data/models.py
+++ data/models.py
@@ -3,9 +3,9 @@ from hierarchy.models import School
 
 
 class SchoolData(models.Model):
-    EMIS_id = models.ForeignKey('School',
-                                related_name='EMIS_id',
-                                verbose_name=u'EMIS Number')
+    EMIS_id_school_data = models.ForeignKey('hierarchy.School',
+                                            related_name='EMIS_id_school_data',
+                                            verbose_name=u'EMIS Number')
     classroom_total = models.IntegerField()
     teachers_total = models.IntegerField()
     g1_teachers = models.IntegerField()
@@ -16,7 +16,7 @@ class SchoolData(models.Model):
     g2_girls = models.IntegerField()
 
     def __unicode__(self):
-        return self.EMIS_id
+        return "%s" % self.EMIS_id_school_data
 
     class Meta:
         verbose_name_plural = "School Data"
@@ -27,16 +27,16 @@ class HeadTeacher(models.Model):
     last_name = models.CharField(max_length=50, verbose_name=u'Last Name')
     gender = models.CharField(max_length=2, verbose_name=u'Gender',
                               choices =(("M", "Male"), ("F", "Female")))
-    mobile_number = models.IntegerField()
+    mobile_number = models.CharField(max_length=15)
     date_of_birth = models.DateField()
     is_zonal_head = models.BooleanField()
-    EMIS_id = models.ForeignKey(School,
-                                related_name="EMIS_id",
-                                verbose_name=u'EMIS Number')
+    EMIS_id_header_teacher = models.ForeignKey(School,
+                                               related_name="EMIS_id_header_teacher",
+                                               verbose_name=u'EMIS Number')
     year = models.DateField()
 
     def __unicode__(self):
-        return "%s %S" % (self.first_name, self.last_name)
+        return "%s %s" % (self.first_name, self.last_name)
 
     class Meta:
         verbose_name_plural = "Head Teacher"
