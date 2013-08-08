b227c5fb4b4f11e657dc27dffa5b99482c172974
Mark Gituma <gitumarkk@gmail.com>
2013-08-08 02:37:59 +0200

    schemamigrate model changes

--- hierarchy/migrations/0001_initial.py
+++ hierarchy/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(SchemaMigration):
         db.create_table(u'hierarchy_district', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('province_id', self.gf('django.db.models.fields.related.ForeignKey')(related_name='province_id', to=orm['hierarchy.Province'])),
+            ('province', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['hierarchy.Province'])),
         ))
         db.send_create_signal(u'hierarchy', ['District'])
 
@@ -27,7 +27,7 @@ class Migration(SchemaMigration):
         db.create_table(u'hierarchy_zone', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('district_id', self.gf('django.db.models.fields.related.ForeignKey')(related_name='district_id', to=orm['hierarchy.District'])),
+            ('district', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['hierarchy.District'])),
         ))
         db.send_create_signal(u'hierarchy', ['Zone'])
 
@@ -35,7 +35,7 @@ class Migration(SchemaMigration):
         db.create_table(u'hierarchy_school', (
             ('EMIS', self.gf('django.db.models.fields.IntegerField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('zone_id', self.gf('django.db.models.fields.related.ForeignKey')(related_name='zone_id', to=orm['hierarchy.Zone'])),
+            ('zone', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['hierarchy.Zone'])),
         ))
         db.send_create_signal(u'hierarchy', ['School'])
 
@@ -59,7 +59,7 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'District'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'province_id': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'province_id'", 'to': u"orm['hierarchy.Province']"})
+            'province': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.Province']"})
         },
         u'hierarchy.province': {
             'Meta': {'object_name': 'Province'},
@@ -70,11 +70,11 @@ class Migration(SchemaMigration):
             'EMIS': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
             'Meta': {'object_name': 'School'},
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'zone_id': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'zone_id'", 'to': u"orm['hierarchy.Zone']"})
+            'zone': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.Zone']"})
         },
         u'hierarchy.zone': {
             'Meta': {'object_name': 'Zone'},
-            'district_id': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'district_id'", 'to': u"orm['hierarchy.District']"}),
+            'district': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.District']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
