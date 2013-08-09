110b446dfbc11a1fb4ccdfec2bc13678cfe85054
Mark Gituma <gitumarkk@gmail.com>
2013-08-09 19:43:08 +0200

    making unittests

--- data/tests/test_api.py
+++ data/tests/test_api.py
@@ -19,3 +19,24 @@ class TestDataAPI(TestCase):
         json_item = json.loads(response.content)
         self.assertIn("meta", json_item)
         self.assertIn("objects", json_item)
+
+    def test_post_data(self):
+        """
+            Testing basic API functionality.
+        """
+        url = reverse('api_dispatch_list',
+                      kwargs={'resource_name': 'data/headteacher',
+                      'api_name': 'api'})
+        response = self.client.post(url,
+                                    format="json",
+                                    data={"name_first": "head",
+                                    "name_last": "teacher",
+                                    "year": "2012-10-12T10:00:00Z",
+                                    "date_of_birth": "2012-10-12T10:00:00Z",
+                                    })
+        print json.loads(response.content)
+        self.assertEqual("application/json", response["Content-Type"])
+        self.assertEqual(response.status_code, 200)
+        json_item = json.loads(response.content)
+        self.assertIn("meta", json_item)
+        self.assertIn("objects", json_item)
