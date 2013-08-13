b89678b09561d35e1f21d13736b898f36d3c83b3
Mark Gituma <gitumarkk@gmail.com>
2013-08-13 16:34:16 +0200

    Registered InboundSMS with admin

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import (SchoolData, HeadTeacher)
+from models import (SchoolData, HeadTeacher, InboundSMS)
+from actions import export_as_csv_action
 
 
 class SchoolDataAdmin(admin.ModelAdmin):
@@ -8,7 +9,13 @@ class SchoolDataAdmin(admin.ModelAdmin):
 
 class HeadTeacherAdmin(admin.ModelAdmin):
     list_display = ["emis", "first_name", "last_name", "msisdn"]
+    actions = [export_as_csv_action("Export selected objects as CSV file")]
+
+
+class InboundSMSAdmin(admin.ModelAdmin):
+    list_display = ["message", "created_by", "created_at"]
 
 
 admin.site.register(SchoolData, SchoolDataAdmin)
 admin.site.register(HeadTeacher, HeadTeacherAdmin)
+admin.site.register(InboundSMS, InboundSMSAdmin)
