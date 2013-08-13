3ce02dd85917b5482033c9eb3da3d641fb2adc89
Mark Gituma <gitumarkk@gmail.com>
2013-08-13 15:44:26 +0200

    added the download as CSV action to the headteacher admin

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from models import (SchoolData, HeadTeacher)
+from actions import export_as_csv_action
 
 
 class SchoolDataAdmin(admin.ModelAdmin):
@@ -8,6 +9,7 @@ class SchoolDataAdmin(admin.ModelAdmin):
 
 class HeadTeacherAdmin(admin.ModelAdmin):
     list_display = ["emis", "first_name", "last_name", "msisdn"]
+    actions = [export_as_csv_action("Export selected objects as CSV file"]
 
 
 admin.site.register(SchoolData, SchoolDataAdmin)
