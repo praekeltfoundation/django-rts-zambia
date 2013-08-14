0c90b1d9b8f6a1c4b065a8a8302e2b6d07982856
Mark Gituma <gitumarkk@gmail.com>
2013-08-14 17:09:58 +0200

    registering academic achievement to admin

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import (SchoolData, HeadTeacher, InboundSMS,
+from models import (SchoolData, HeadTeacher, InboundSMS, AcademicAchievementCode,
                     TeacherPerfomanceData, LearnerPerfomanceData)
 from actions import export_as_csv_action
 
@@ -26,9 +26,13 @@ class InboundSMSAdmin(admin.ModelAdmin):
     list_display = ["message", "created_by", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
 
+class AcademicAchievementCodeAdmin(admin.ModelAdmin):
+    list_display = ["id", "achievement"]
+
 
 admin.site.register(SchoolData, SchoolDataAdmin)
 admin.site.register(HeadTeacher, HeadTeacherAdmin)
 admin.site.register(TeacherPerfomanceData, TeacherPerfomanceDataAdmin)
 admin.site.register(LearnerPerfomanceData, LearnerPerfomanceDataAdmin)
 admin.site.register(InboundSMS, InboundSMSAdmin)
+admin.site.register(AcademicAchievementCode, AcademicAchievementCodeAdmin)
