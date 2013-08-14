c2f55ac35c0543ee1df81cda253848beecf0ee89
Mark Gituma <gitumarkk@gmail.com>
2013-08-14 18:17:06 +0200

    Registered Academic achievement with urls.py

--- data/urls.py
+++ data/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 from data.api import (HeadTeacherResource, SchoolDataResource, InboundSMSResource,
-                      TeacherPerfomanceDataResource, LearnerPerfomanceDataResource)
+                      TeacherPerfomanceDataResource, LearnerPerfomanceDataResource,
+                      AcademicAchievementCodeResource)
 from tastypie.api import Api
 
 
@@ -12,6 +13,7 @@ api_resources.register(SchoolDataResource())
 api_resources.register(TeacherPerfomanceDataResource())
 api_resources.register(LearnerPerfomanceDataResource())
 api_resources.register(InboundSMSResource())
+api_resources.register(AcademicAchievementCodeResource())
 api_resources.prepend_urls()
 
 # Setting the urlpatterns to hook into the api urls
