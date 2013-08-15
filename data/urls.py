31c099049e1fdb6e41038a358ac9bd062c08e38d
Mark Gituma <gitumarkk@gmail.com>
2013-08-15 17:09:29 +0200

    changed api name to v1

--- data/urls.py
+++ data/urls.py
@@ -7,7 +7,7 @@ from tastypie.api import Api
 
 # Setting the API base name and registering the API resources using
 # Tastypies API function
-api_resources = Api(api_name='api')
+api_resources = Api(api_name='v1')
 api_resources.register(HeadTeacherResource())
 api_resources.register(SchoolDataResource())
 api_resources.register(TeacherPerfomanceDataResource())
@@ -18,5 +18,5 @@ api_resources.prepend_urls()
 
 # Setting the urlpatterns to hook into the api urls
 urlpatterns = patterns('',
-    url(r'^', include(api_resources.urls))
+    url(r'^api/', include(api_resources.urls))
 )
