08993817f3cf31682c5a2fd91e0ce8e52a1974a3
Mark Gituma <gitumarkk@gmail.com>
2013-08-19 17:37:42 +0200

    added queryset filtering for admin

--- data/admin.py
+++ data/admin.py
@@ -9,25 +9,73 @@ class SchoolDataAdmin(admin.ModelAdmin):
     list_display = ["emis", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
 
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(SchoolDataAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(emis__zone__district=request.user.userdistrict.district_id)
+
 
 class HeadTeacherAdmin(admin.ModelAdmin):
-    list_display = ["emis", "first_name", "last_name", "msisdn", "created_at"]
+    list_display = ["id", "emis", "first_name", "last_name", "msisdn", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
 
+
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(HeadTeacherAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        print request.user.userdistrict.district_id;
+        print dir(request.user.userdistrict);
+        return qs.filter(emis__zone__district=request.user.userdistrict.district_id)
 
 class TeacherPerfomanceDataAdmin(admin.ModelAdmin):
     list_display = ["emis", "created_by", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
+
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(TeacherPerfomanceDataAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(emis__zone__district=request.user.userdistrict.district_id)
 
 
 class LearnerPerfomanceDataAdmin(admin.ModelAdmin):
     list_display = ["emis", "created_by", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
 
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(LearnerPerfomanceDataAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(emis__zone__district=request.user.userdistrict.district_id)
+
 
 class InboundSMSAdmin(admin.ModelAdmin):
     list_display = ["message", "created_by", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file")]
+
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(InboundSMSAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(created_by__emis__zone__district=request.user.userdistrict.district_id)
+
 
 class AcademicAchievementCodeAdmin(admin.ModelAdmin):
     list_display = ["id", "achievement"]
