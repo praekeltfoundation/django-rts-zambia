90509a1a4db28efd37f8366b5182879888763e29
Mark Gituma <gitumarkk@gmail.com>
2013-08-19 17:37:19 +0200

    added filtering for schools in hierarchy

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -4,6 +4,15 @@ from models import (Province, District, Zone, School)
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ["emis", "name", "zone"]
 
+    def queryset(self, request):
+        """
+        Limits queries for pages that belong to district admin
+        """
+        qs = super(SchoolAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(zone__district=request.user.userdistrict.district_id)
+
 admin.site.register(Province)
 admin.site.register(District)
 admin.site.register(Zone)
