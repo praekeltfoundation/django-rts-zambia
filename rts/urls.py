4d2ccd570a34dbd79adbdb2168ca1df4370c69d8
Simon Cross <hodgestar@gmail.com>
2013-08-19 19:47:31 +0200

    Use reverse_lazy to avoid weird url setup circularity.

--- rts/urls.py
+++ rts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
 
 from django.contrib import admin
@@ -7,7 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', RedirectView.as_view(url=reverse('admin:index')), name='home'),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index')),
+        name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^', include('hierarchy.urls')),
