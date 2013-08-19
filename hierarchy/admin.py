a95fdc0a1c3702f9d8c435c049841821d63318d0
Mark Gituma <gitumarkk@gmail.com>
2013-08-19 15:28:57 +0200

    added  verbose name to hierarchy

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from models import (Province, District, Zone, School)
 
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ["name", "province"]
+
+
+class ZoneAdmin(admin.ModelAdmin):
+    list_display = ["name", "district"]
+
+
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ["emis", "name", "zone"]
 
-admin.site.register(Province)
-admin.site.register(District)
-admin.site.register(Zone)
+
+admin.site.register(Province, ProvinceAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Zone, ZoneAdmin)
 admin.site.register(School, SchoolAdmin)
