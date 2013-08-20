b91b7ecc3279ce740bde2ecb7ab5299b5c399d7c
Mark Gituma <gitumarkk@gmail.com>
2013-08-20 10:47:57 +0200

    Working on unittests for users

--- users/tests/test_admin.py
+++ users/tests/test_admin.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 
-class TestAdminVArea(TestCase):
+class TestAdminView(TestCase):
     fixtures = ['test_hierarchy.json', 'test_data.json']
 
     def setUp(self):
@@ -14,7 +14,7 @@ class TestAdminVArea(TestCase):
         self.districts = ['Solwezi', 'Mongu', 'CHIPATA', 'CHINSALI', 'Mansa', 'Mporokoso']
         self.zones = ['Kikombe', 'LUKWETA', 'MADZIMOYO', 'KALELA', 'Mbaso', 'Bweupe']
         self.schools = [3633, 14090, 8682, 2064, 1774, 2525]
-    def test_admin_can_view_schools(self):
+    def test_admin_can_view_all(self):
         self.client.login(username=self.admin.username,
                           password="pass123")
 
@@ -51,3 +51,59 @@ class TestAdminVArea(TestCase):
         school = []
         [school.append(k.emis) for k in school_result]
         self.assertEqual(sorted(school), sorted(self.schools))
+
+
+
+class TestDistrictAdmin1Area(TestCase):
+    fixtures = ['test_hierarchy.json', 'test_data.json']
+
+    def setUp(self):
+        self.d1 = User.objects.create_user('district_admin1',
+                                            'district_admin@test.com', 
+                                            'pass123',
+                                            )
+        self.provinces = ['North Western', 'WESTERN', 'EASTERN', 'MUCHINGA', 'Luapula', 'Northern']
+        self.districts = ['Solwezi', 'Mongu', 'CHIPATA', 'CHINSALI', 'Mansa', 'Mporokoso']
+        self.zones = ['Kikombe', 'LUKWETA', 'MADZIMOYO', 'KALELA', 'Mbaso', 'Bweupe']
+        self.schools = [3633, 14090, 8682, 2064, 1774, 2525]
+    def test_admin_can_view_all(self):
+        print self.d1.__dict__['_state']
+        print dir(self.d1)
+        print self.d1.userdistrict
+        
+        # self.client.login(username=self.admin.username,
+        #                   password="pass123")
+
+        # url_index = reverse("admin:index")
+        # response = self.client.get(url_index)
+
+        # url = reverse("admin:hierarchy_province_changelist")
+        # response = self.client.get(url)
+        # province_result = response.__dict__['context_data']['cl'].__dict__['result_list']
+        # province = []
+        # [province.append(k.name) for k in province_result]
+        # self.assertEqual(sorted(province), sorted(self.provinces))
+
+
+        # url = reverse("admin:hierarchy_district_changelist")
+        # response = self.client.get(url)
+        # district_result = response.__dict__['context_data']['cl'].__dict__['result_list']
+        # district = []
+        # [district.append(k.name) for k in district_result]
+        # self.assertEqual(sorted(district), sorted(self.districts))
+
+
+        # url = reverse("admin:hierarchy_zone_changelist")
+        # response = self.client.get(url)
+        # zone_result =  response.__dict__['context_data']['cl'].__dict__['result_list']
+        # zone = []
+        # [zone.append(k.name) for k in zone_result]
+        # self.assertEqual(sorted(zone), sorted(self.zones))
+
+
+        # url = reverse("admin:hierarchy_school_changelist")
+        # response = self.client.get(url)
+        # school_result = response.__dict__['context_data']['cl'].__dict__['result_list']
+        # school = []
+        # [school.append(k.emis) for k in school_result]
+        # self.assertEqual(sorted(school), sorted(self.schools))
