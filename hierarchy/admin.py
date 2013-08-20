49864cb73220a7a0b4608a011fc3915d856a6c67
Mark Gituma <gitumarkk@gmail.com>
2013-08-20 16:38:49 +0200

    added code for filtering by district_id

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from models import (Province, District, Zone, School)
+from users.models import UserDistrict
 
 class ProvinceAdmin(admin.ModelAdmin):
     list_display = ["name"]
@@ -24,7 +25,10 @@ class SchoolAdmin(admin.ModelAdmin):
         qs = super(SchoolAdmin, self).queryset(request)
         if request.user.is_superuser:
             return qs
-        return qs.filter(zone__district=request.user.userdistrict.district_id)
+        elif UserDistrict.objects.filter(user_id=request.user.id).exists():
+            return qs.filter(zone__district=request.user.userdistrict.district_id)
+        else:
+            return qs
 
 
 admin.site.register(Province, ProvinceAdmin)
