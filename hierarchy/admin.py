d25ff7a2dc0497c20ac590ae5f1ab0fa385ae51c
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 17:18:34 +0200

    added Managepermissions inheritance form shared utils

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -3,43 +3,19 @@ from models import (Province, District, Zone, School)
 from rts.utils import DistrictIdFilter, ManagePermissions
 
 
-class ProvinceAdmin(admin.ModelAdmin):
+class ProvinceAdmin(ManagePermissions):
     list_display = ["name"]
 
-    def get_actions(self, request):
-        actions = super(ProvinceAdmin, self).get_actions(request)
-        delete_perm = super(ProvinceAdmin, self).has_delete_permission(request)
-        return ManagePermissions(parent=self,
-                                 request=request,
-                                 actions=actions,
-                                 delete_perm=delete_perm).remove_delete_permission()
 
-
-class DistrictAdmin(admin.ModelAdmin):
+class DistrictAdmin(ManagePermissions):
     list_display = ["name", "province"]
 
-    def get_actions(self, request):
-        actions = super(DistrictAdmin, self).get_actions(request)
-        delete_perm = super(DistrictAdmin, self).has_delete_permission(request)
-        return ManagePermissions(parent=self,
-                                 request=request,
-                                 actions=actions,
-                                 delete_perm=delete_perm).remove_delete_permission()
 
-
-class ZoneAdmin(admin.ModelAdmin):
+class ZoneAdmin(ManagePermissions):
     list_display = ["name", "district"]
 
-    def get_actions(self, request):
-        actions = super(ZoneAdmin, self).get_actions(request)
-        delete_perm = super(ZoneAdmin, self).has_delete_permission(request)
-        return ManagePermissions(parent=self,
-                                 request=request,
-                                 actions=actions,
-                                 delete_perm=delete_perm).remove_delete_permission()
 
-
-class SchoolAdmin(admin.ModelAdmin):
+class SchoolAdmin(ManagePermissions):
     list_display = ["emis", "name", "zone"]
 
 
@@ -49,15 +25,6 @@ class SchoolAdmin(admin.ModelAdmin):
         """
         qs = super(SchoolAdmin, self).queryset(request)
         return DistrictIdFilter(parent=self, request=request, qs=qs).queryset()
-
-
-    def get_actions(self, request):
-        actions = super(SchoolAdmin, self).get_actions(request)
-        delete_perm = super(SchoolAdmin, self).has_delete_permission(request)
-        return ManagePermissions(parent=self,
-                                 request=request,
-                                 actions=actions,
-                                 delete_perm=delete_perm).remove_delete_permission()
 
 
 admin.site.register(Province, ProvinceAdmin)
