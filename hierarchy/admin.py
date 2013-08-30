3a6221e9ca43eb68497b947cefb0f3a562efbe9a
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 17:13:42 +0200

    created manage permissions view

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -1,18 +1,42 @@
 from django.contrib import admin
 from models import (Province, District, Zone, School)
-from rts.utils import DistrictIdFilter
+from rts.utils import DistrictIdFilter, ManagePermissions
 
 
 class ProvinceAdmin(admin.ModelAdmin):
     list_display = ["name"]
 
+    def get_actions(self, request):
+        actions = super(ProvinceAdmin, self).get_actions(request)
+        delete_perm = super(ProvinceAdmin, self).has_delete_permission(request)
+        return ManagePermissions(parent=self,
+                                 request=request,
+                                 actions=actions,
+                                 delete_perm=delete_perm).remove_delete_permission()
+
 
 class DistrictAdmin(admin.ModelAdmin):
     list_display = ["name", "province"]
 
+    def get_actions(self, request):
+        actions = super(DistrictAdmin, self).get_actions(request)
+        delete_perm = super(DistrictAdmin, self).has_delete_permission(request)
+        return ManagePermissions(parent=self,
+                                 request=request,
+                                 actions=actions,
+                                 delete_perm=delete_perm).remove_delete_permission()
+
 
 class ZoneAdmin(admin.ModelAdmin):
     list_display = ["name", "district"]
+
+    def get_actions(self, request):
+        actions = super(ZoneAdmin, self).get_actions(request)
+        delete_perm = super(ZoneAdmin, self).has_delete_permission(request)
+        return ManagePermissions(parent=self,
+                                 request=request,
+                                 actions=actions,
+                                 delete_perm=delete_perm).remove_delete_permission()
 
 
 class SchoolAdmin(admin.ModelAdmin):
@@ -25,6 +49,15 @@ class SchoolAdmin(admin.ModelAdmin):
         """
         qs = super(SchoolAdmin, self).queryset(request)
         return DistrictIdFilter(parent=self, request=request, qs=qs).queryset()
+
+
+    def get_actions(self, request):
+        actions = super(SchoolAdmin, self).get_actions(request)
+        delete_perm = super(SchoolAdmin, self).has_delete_permission(request)
+        return ManagePermissions(parent=self,
+                                 request=request,
+                                 actions=actions,
+                                 delete_perm=delete_perm).remove_delete_permission()
 
 
 admin.site.register(Province, ProvinceAdmin)
