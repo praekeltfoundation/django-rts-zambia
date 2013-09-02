6d5c53859371e1c217b4beb55488b7c5a0082313
Mark Gituma <gitumarkk@gmail.com>
2013-09-02 13:25:30 +0200

    added new download function for CSV Files

--- data/actions.py
+++ data/actions.py
@@ -2,14 +2,29 @@ import csv
 from django.http import HttpResponse
 
 
-def export_as_csv_action(description="Export selected records to CSV file",
+# From http://djangosnippets.org/snippets/2712/
+def export_select_fields_csv_action(description="Export selected objects as CSV file",
                          fields=None, exclude=None, header=True):
     """
     This function returns an export csv action
-    'fields' and 'exclude' work like in django ModelForm
+
+    'fields' is a list of tuples denoting the field and label to be exported. Labels
+    make up the header row of the exported file if header=True.
+
+        fields=[
+                ('field1', 'label1'),
+                ('field2', 'label2'),
+                ('field3', 'label3'),
+            ]
+
+    'exclude' is a flat list of fields to exclude. If 'exclude' is passed,
+    'fields' will not be used. Either use 'fields' or 'exclude.'
+
+        exclude=['field1', 'field2', field3]
+
     'header' is whether or not to output the column names as the first row
 
-    from http://djangosnippets.org/snippets/2369/
+    Based on: http://djangosnippets.org/snippets/2020/
     """
     def export_as_csv(modeladmin, request, queryset):
         """
@@ -17,24 +32,25 @@ def export_as_csv_action(description="Export selected records to CSV file",
         based on http://djangosnippets.org/snippets/1697/
         """
         opts = modeladmin.model._meta
-        field_names = set([field.name for field in opts.fields])
-        if fields:
-            fieldset = set(fields)
-            field_names = field_names & fieldset
-        elif exclude:
-            excludeset = set(exclude)
-            field_names = field_names - excludeset
+        field_names = [field.name for field in opts.fields]
+        labels = []
+        if exclude:
+            field_names = [v for v in field_names if v not in exclude]
+        elif fields:
+            field_names = [k for k, v in fields if k in field_names]
+            labels = [v for k, v in fields if k in field_names]
 
         response = HttpResponse(mimetype='text/csv')
-        response['Content-Disposition'] = ('attachment; filename=%s.csv'
-                                           % unicode(opts).replace('.', '_'))
+        response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
 
         writer = csv.writer(response)
         if header:
-            writer.writerow(list(field_names))
+            if labels:
+                writer.writerow(labels)
+            else:
+                writer.writerow(field_names)
         for obj in queryset:
-            writer.writerow([unicode(getattr(obj, field)).encode("utf-8", "replace")
-                            for field in field_names])
+            writer.writerow([unicode(getattr(obj, field)).encode('utf-8') for field in field_names])
         return response
     export_as_csv.short_description = description
     return export_as_csv
