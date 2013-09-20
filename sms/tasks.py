9de916e3449be1edefe91fa9bdcb876bad89444d
gitumarkk <gitumarkk@gmail.com>
2013-09-20 17:39:40 +0200

    added api configuration variables to pythonpath

--- sms/tasks.py
+++ sms/tasks.py
@@ -1,22 +1,21 @@
 from celery.decorators import task
 from hierarchy.models import Zone
 from sms.sms_sender import VumiGoSender
+from django.conf import settings
 
 
 @task
 def send_sms(msisdn, sms):
-    sms = VumiGoSender(api_url="",
-                       account_id="",
-                       conversation_id="",
-                       conversation_token="")
-
-    sms.send_sms(msisdn, sms)
+    sms = VumiGoSender(api_url=settings.API_URL,
+                       account_id=settings.ACCOUNT_ID,
+                       conversation_id=settings.CONVERSATION_ID,
+                       conversation_token=settings.CONVERSATION_TOKEN)
+    # sms.send_sms(msisdn, sms)
 
 
 @task
 def task_query_zone(id, sms):
     school_set = Zone.objects.get(id=id).school_set.all()
-
     for school in school_set:
         headteacher_set = school.headteacher_set.all()
         for headteacher in headteacher_set:
