ae158a5288e80897fc52e562e81950967c1a58be
gitumarkk <gitumarkk@gmail.com>
2013-10-30 15:48:51 +0200

    copy and deleting any none data

---
+++ data/migrations/0014_delete_none_field.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Don't use "from appname.models import ModelName".
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+
+        none_emis = orm.HeadTeacher.objects.filter(emis=None)
+
+        for data in none_emis.all():
+            store = orm.HeadTeacherDuplicateStore(first_name=data.first_name,
+                                                  last_name=data.last_name,
+                                                  gender=data.gender,
+                                                  msisdn=data.msisdn,
+                                                  date_of_birth=data.date_of_birth,
+                                                  is_zonal_head=data.is_zonal_head,
+                                                  zonal_head_name=data.zonal_head_name,
+                                                  created_at=data.created_at,
+                                                  emis=data.emis,
+                                                  origin_id=data.id
+                                                  )
+            store.save()
+
+            if data.schooldata_set.all():
+                store2 = orm.SchoolDataDuplicateStore(emis=data.schooldata_set.all()[0].emis,
+                                                      name=data.schooldata_set.all()[0].name,
+                                                      classrooms=data.schooldata_set.all()[0].classrooms,
+                                                      teachers=data.schooldata_set.all()[0].teachers,
+                                                      teachers_g1=data.schooldata_set.all()[0].teachers_g1,
+                                                      teachers_g2=data.schooldata_set.all()[0].teachers_g2,
+                                                      boys_g2=data.schooldata_set.all()[0].boys_g2,
+                                                      girls_g2=data.schooldata_set.all()[0].girls_g2,
+                                                      created_at=data.schooldata_set.all()[0].created_at,
+                                                      created_by=orm.HeadTeacherDuplicateStore.objects.get(origin_id=data.id),
+                                                      origin_id=data.schooldata_set.all()[0].id
+                                                      )
+                store2.save()
+        none_emis.delete()
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'data.academicachievementcode': {
+            'Meta': {'object_name': 'AcademicAchievementCode'},
+            'achievement': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'data.headteacher': {
+            'Meta': {'object_name': 'HeadTeacher'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']", 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_zonal_head': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'msisdn': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'zonal_head_name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'data.headteacherduplicatestore': {
+            'Meta': {'object_name': 'HeadTeacherDuplicateStore'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']", 'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_zonal_head': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'msisdn': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'origin_id': ('django.db.models.fields.IntegerField', [], {}),
+            'zonal_head_name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'data.inboundsms': {
+            'Meta': {'object_name': 'InboundSMS'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.HeadTeacher']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        u'data.learnerperformancedata': {
+            'Meta': {'object_name': 'LearnerPerformanceData'},
+            'below_minimum_results': ('django.db.models.fields.IntegerField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.HeadTeacher']"}),
+            'desirable_results': ('django.db.models.fields.IntegerField', [], {}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']"}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'minimum_results': ('django.db.models.fields.IntegerField', [], {}),
+            'outstanding_results': ('django.db.models.fields.IntegerField', [], {}),
+            'phonetic_awareness': ('django.db.models.fields.IntegerField', [], {}),
+            'reading_comprehension': ('django.db.models.fields.IntegerField', [], {}),
+            'total_number_pupils': ('django.db.models.fields.IntegerField', [], {}),
+            'vocabulary': ('django.db.models.fields.IntegerField', [], {}),
+            'writing_diction': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'data.schooldata': {
+            'Meta': {'object_name': 'SchoolData'},
+            'boys_g2': ('django.db.models.fields.IntegerField', [], {}),
+            'classrooms': ('django.db.models.fields.IntegerField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.HeadTeacher']"}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']"}),
+            'girls_g2': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'teachers': ('django.db.models.fields.IntegerField', [], {}),
+            'teachers_g1': ('django.db.models.fields.IntegerField', [], {}),
+            'teachers_g2': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'data.schooldataduplicatestore': {
+            'Meta': {'object_name': 'SchoolDataDuplicateStore'},
+            'boys_g2': ('django.db.models.fields.IntegerField', [], {}),
+            'classrooms': ('django.db.models.fields.IntegerField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.HeadTeacherDuplicateStore']"}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']"}),
+            'girls_g2': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'origin_id': ('django.db.models.fields.IntegerField', [], {}),
+            'teachers': ('django.db.models.fields.IntegerField', [], {}),
+            'teachers_g1': ('django.db.models.fields.IntegerField', [], {}),
+            'teachers_g2': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'data.teacherperformancedata': {
+            'Meta': {'object_name': 'TeacherPerformanceData'},
+            'academic_level': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.AcademicAchievementCode']"}),
+            'age': ('django.db.models.fields.IntegerField', [], {}),
+            'attitudes_and_beliefs': ('django.db.models.fields.IntegerField', [], {}),
+            'classroom_environment_score': ('django.db.models.fields.IntegerField', [], {}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['data.HeadTeacher']"}),
+            'emis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.School']"}),
+            'g2_pupils_present': ('django.db.models.fields.IntegerField', [], {}),
+            'g2_pupils_registered': ('django.db.models.fields.IntegerField', [], {}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pupil_engagement_score': ('django.db.models.fields.IntegerField', [], {}),
+            'pupils_books_number': ('django.db.models.fields.IntegerField', [], {}),
+            'pupils_materials_score': ('django.db.models.fields.IntegerField', [], {}),
+            'reading_assessment': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'reading_lesson': ('django.db.models.fields.IntegerField', [], {}),
+            'reading_total': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            't_l_materials': ('django.db.models.fields.IntegerField', [], {}),
+            'training_subtotal': ('django.db.models.fields.IntegerField', [], {}),
+            'ts_number': ('django.db.models.fields.IntegerField', [], {}),
+            'years_experience': ('django.db.models.fields.CharField', [], {'max_length': '5'})
+        },
+        u'hierarchy.district': {
+            'Meta': {'object_name': 'District'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'province': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.Province']"})
+        },
+        u'hierarchy.province': {
+            'Meta': {'object_name': 'Province'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'hierarchy.school': {
+            'Meta': {'object_name': 'School'},
+            'emis': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'max_length': '8'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'zone': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.Zone']"})
+        },
+        u'hierarchy.zone': {
+            'Meta': {'object_name': 'Zone'},
+            'district': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['hierarchy.District']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['data']
+    symmetrical = True
