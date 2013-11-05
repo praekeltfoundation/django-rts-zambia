abefec41b95056f7104e61f3cdd53b397fd64864
gitumarkk <gitumarkk@gmail.com>
2013-11-05 16:06:30 +0200

    added docstring

--- data/migrations/0013_delete_teacherperfomance_learnerperformance_data.py
+++ data/migrations/0013_delete_teacherperfomance_learnerperformance_data.py
@@ -7,10 +7,11 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName".
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
+        """ Write your forwards methods here.
+        Note: Don't use "from appname.models import ModelName".
+        Use orm.ModelName to refer to models in this application,
+        and orm['appname.ModelName'] for models in other applications.
+        """
 
         orm.TeacherPerformanceData.objects.all().delete()
         orm.LearnerPerformanceData.objects.all().delete()
