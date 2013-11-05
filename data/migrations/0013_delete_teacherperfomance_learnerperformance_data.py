606d193462fc080be9485a65079f6bfc5723eeea
gitumarkk <gitumarkk@gmail.com>
2013-11-05 16:49:39 +0200

    adding docstring describing the code

--- data/migrations/0013_delete_teacherperfomance_learnerperformance_data.py
+++ data/migrations/0013_delete_teacherperfomance_learnerperformance_data.py
@@ -7,10 +7,8 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        """ Write your forwards methods here.
-        Note: Don't use "from appname.models import ModelName".
-        Use orm.ModelName to refer to models in this application,
-        and orm['appname.ModelName'] for models in other applications.
+        """
+        Deleting the Teacher and Learner Performance Data
         """
 
         orm.TeacherPerformanceData.objects.all().delete()
