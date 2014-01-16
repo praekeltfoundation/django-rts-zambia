f9e4f3807cf03b73fcbe1f2ddd75306320b8565c
gitumarkk <gitumarkk@gmail.com>
2014-01-16 18:16:37 +0200

    added api to urls

--- data/urls.py
+++ data/urls.py
@@ -1,19 +1,27 @@
 from django.conf.urls import patterns, url, include
-from data.api import (HeadTeacherResource, SchoolDataResource, InboundSMSResource,
-                      TeacherPerformanceDataResource, LearnerPerformanceDataResource,
-                      AcademicAchievementCodeResource)
+from data import api
 from tastypie.api import Api
 
 
 # Setting the API base name and registering the API resources using
 # Tastypies API function
 api_resources = Api(api_name='v1')
-api_resources.register(HeadTeacherResource())
-api_resources.register(SchoolDataResource())
-api_resources.register(TeacherPerformanceDataResource())
-api_resources.register(LearnerPerformanceDataResource())
-api_resources.register(InboundSMSResource())
-api_resources.register(AcademicAchievementCodeResource())
+api_resources.register(api.HeadTeacherResource())
+api_resources.register(api.SchoolDataResource())
+api_resources.register(api.TeacherPerformanceDataResource())
+api_resources.register(api.LearnerPerformanceDataResource())
+api_resources.register(api.InboundSMSResource())
+api_resources.register(api.AcademicAchievementCodeResource())
+
+
+api_resources.register(api.HeadTeacherCSVDownloadResource())
+api_resources.register(api.SchoolDataCSVDownloadResource())
+api_resources.register(api.TeacherPerformanceDataCSVDownloadResource())
+api_resources.register(api.LearnerPerformanceDataCSVDownloadResource())
+api_resources.register(api.InboundSMSCSVDownloadResource())
+api_resources.register(api.AcademicAchievementCodeCSVDownloadResource())
+
+
 api_resources.prepend_urls()
 
 # Setting the urlpatterns to hook into the api urls
