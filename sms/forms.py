ff5d3c719e4a0a95e40b2b96bb865a8e14edab74
gitumarkk <gitumarkk@gmail.com>
2014-01-27 17:44:02 +0200

    Updated te ChooseDistrictFomr to account for cleaned data and sms field

--- sms/forms.py
+++ sms/forms.py
@@ -2,16 +2,22 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 
 
-class ChooseDistricsForms(forms.Form):
-    send_to_all = forms.BooleanField(label="All", required=False)
+class ChooseDistricsForm(forms.Form):
+    sms = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+    send_to_all = forms.BooleanField(label=_("All"), required=False)
 
     def __init__(self, *args, **kwargs):
         queryset = kwargs.pop("queryset", None)
-        super(ChooseDistricsForms, self).__init__(*args, **kwargs)
+        super(ChooseDistricsForm, self).__init__(*args, **kwargs)
         for query in queryset:
-            self.fields["district_%s" % query.id ] = forms.BooleanField(label=query.name, required=False)
+            self.fields["%s" % query.id ] = forms.BooleanField(label=query.name, required=False)
 
+    def clean(self):
+        cleaned_data = super(ChooseDistricsForm, self).clean()
 
-class ChooseDistricsFormSet(forms.Form):
-    district_id = forms.CharField(label=_(""), widget=forms.HiddenInput())
-    send_to = forms.BooleanField(label="All", required=False)
+        data = dict((k, v) for k, v in cleaned_data.iteritems() if v and k != "sms")
+
+        if "send_to_all" in data and len(data) > 1:
+            raise forms.ValidationError(_("Choose all or specific zones not both"))
+
+        return cleaned_data
