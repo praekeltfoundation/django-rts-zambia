c1fda171470d9f5f9ff823b4b61d60f3a4c452e7
gitumarkk <gitumarkk@gmail.com>
2014-01-30 18:19:00 +0200

    wrote function for send to zone view

--- sms/admin.py
+++ sms/admin.py
@@ -12,93 +12,78 @@ from django import forms
 # Project
 from rts.utils import DistrictIdFilter, ManagePermissions
 from rts.actions import export_select_fields_csv_action
-from sms.models import SendSMS, SMSZones, TempSMSZones
+from sms.models import SendSMS, SMSZones
 from hierarchy.models import Zone, District
-from sms.forms import ChooseDistricsForm
+from sms.forms import ChooseDistricsForm, ChooseZonesForm
 from sms.tasks import task_query_zone
 from users.models import UserDistrict
 from data.models import InboundSMS
 
 
+# class SMSZoneForm(forms.ModelForm):
+#     all = forms.BooleanField(label="All", required=False)
 
-"""
-The Code below overides the TempZonelClass thus adding extra variables to it,
-that will be used by form.ModelForm to auto generate the required zones
-dynamically
-"""
-zones = Zone.objects.all()
-zones_dict = dict((obj.id, obj.name) for obj in zones)
-
-for key, value in zones_dict.iteritems():
-    TempSMSZones.add_to_class(str(key), models.BooleanField(value))
+#     class Meta:
+#         model = TempSMSZones
+#         exclude = ["zone",
+#                    "send_sms",
+#                    "num_sent"]
 
 
-class SMSZoneForm(forms.ModelForm):
-    all = forms.BooleanField(label="All", required=False)
+# class SendSMSForm(forms.ModelForm):
 
-    class Meta:
-        model = TempSMSZones
-        exclude = ["zone",
-                   "send_sms",
-                   "num_sent"]
+#     sms = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+#     class Meta:
+#         models = SendSMS
+#         exclude = ["total_sent",
+#                     "replies",
+#                    "user",
+#                    "district",
+#                    "created_at"]
 
-
-class SendSMSForm(forms.ModelForm):
-
-    sms = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
-    class Meta:
-        models = SendSMS
-        exclude = ["total_sent",
-                    "replies",
-                   "user",
-                   "district",
-                   "created_at"]
-
-    def __init__(self, *args, **kwargs):
-        super(SendSMSForm, self).__init__(*args, **kwargs)
+#     def __init__(self, *args, **kwargs):
+#         super(SendSMSForm, self).__init__(*args, **kwargs)
 
 
-class SMSZoneFormset(BaseInlineFormSet):
-    def clean(self):
-        super(SMSZoneFormset, self).clean()
+# class SMSZoneFormset(BaseInlineFormSet):
+#     def clean(self):
+#         super(SMSZoneFormset, self).clean()
 
-        for form in self.forms:
-            if not hasattr(form, 'cleaned_data'):
-                continue
+#         for form in self.forms:
+#             if not hasattr(form, 'cleaned_data'):
+#                 continue
 
-            data = dict((k, v) for k, v in form.cleaned_data.iteritems() if v)
-            if "all" in data and len(data) > 2:
-                raise forms.ValidationError("Choose all or specific zones not both")
+#             data = dict((k, v) for k, v in form.cleaned_data.iteritems() if v)
+#             if "all" in data and len(data) > 2:
+#                 raise forms.ValidationError("Choose all or specific zones not both")
 
 
 
-class SMSZoneInline(admin.StackedInline):
-    model = TempSMSZones
-    form = SMSZoneForm
-    formset = SMSZoneFormset
-    max_num = 1
+# class SMSZoneInline(admin.StackedInline):
+#     model = TempSMSZones
+#     form = SMSZoneForm
+#     formset = SMSZoneFormset
+#     max_num = 1
 
-    def get_fieldsets(self, request, obj=None):
-        fields_array = super(SMSZoneInline, self).get_fieldsets(request, obj=None)
-        fields = fields_array[0][1]
-        result = []
-        temp = []
-        if UserDistrict.objects.filter(user_id=request.user.id).exists():
-            district = District.objects.get(id=request.user.userdistrict.district_id)
-            zones = district.zone_set.all()
-            query_dict = [temp.append(str(obj.id)) for obj in zones]
+#     def get_fieldsets(self, request, obj=None):
+#         fields_array = super(SMSZoneInline, self).get_fieldsets(request, obj=None)
+#         fields = fields_array[0][1]
+#         result = []
+#         temp = []
+#         if UserDistrict.objects.filter(user_id=request.user.id).exists():
+            # district = District.objects.get(id=request.user.userdistrict.district_id)
+            # zones = district.zone_set.all()
+#             query_dict = [temp.append(str(obj.id)) for obj in zones]
 
-        for field in fields["fields"]:
-            if field in temp:
-                result.append(field)
-        result.insert(0, "all")
-        fields_array = [(None, {'fields': result})]
-        return fields_array
+#         for field in fields["fields"]:
+#             if field in temp:
+#                 result.append(field)
+#         result.insert(0, "all")
+#         fields_array = [(None, {'fields': result})]
+#         return fields_array
 
 
 class SendSMSAdmin(ManagePermissions):
-    form = SendSMSForm
-    inlines = [SMSZoneInline]
     list_display = ["sms", "created_by", "total_sent_messages", "replies", "district", "sent_to_all", "created_at"]
     actions = [export_select_fields_csv_action("Export selected objects as CSV file")]
 
@@ -133,41 +118,53 @@ class SendSMSAdmin(ManagePermissions):
         """
         return HttpResponseRedirect(reverse("admin:sms_sendsms_changelist"))
 
-    def save_model(self, request, obj, form, change):
-        """
-        Given a model instance save it to the database.
-        """
-        obj.user = request.user
-        obj.district_id = request.user.userdistrict.district_id
-        obj.save()
+    def zones_view(self, request):
+        # ===============================
+        # Add validation & test for nothing selected
+        # Add test and validation for districts form
+        # ===============================
+        if not self.is_district_admin(request):
+            messages.error(request, "You don't have the correct permissions to view this page")
+            return HttpResponseRedirect(reverse("admin:index"))
 
+        # Replicating views from django ModelAdmin
+        model = self.model
+        opts = model._meta
 
-    def save_related(self, request, form, formsets, change):
-        """
-        Overriding save related to save ro real database instead of the dummy database
-        """
-        form.save_m2m()
-        for formset in formsets:
-            if UserDistrict.objects.filter(user_id=request.user.id).exists():
-                district = District.objects.get(id=request.user.userdistrict.district_id)
-                zones = district.zone_set.all()
-                temp = [str(obj.id) for obj in zones]
+        district = District.objects.get(id=request.user.userdistrict.district_id)
+        zones = district.zone_set.all()
 
-            result = {}
-            for key, value in formset.cleaned_data[0].iteritems():
-                if value:
-                    result[key] = value
+        if request.method == "POST":
+            zone_form = ChooseZonesForm(request.POST, queryset=zones)
 
-            for key, value in result.iteritems():
-                if key.isdigit() and key in temp:
-                    self.custom_save_zones(key, result["temp_sms"])
-                    task_query_zone.delay(int(key), result["temp_sms"].sms)
+            if zone_form.is_valid():
+                if zone_form.cleaned_data["send_to_all"] == True:
+                    self.save_zones_and_send_sms(request,
+                                                 zones,
+                                                 district,
+                                                 zone_form.cleaned_data["sms"],
+                                                 to_all=True)
+                else:
+                    # Iterating over the rest of the form IDs and checking if True and key is a digit
+                    ids = [int(key) for key, value in zone_form.cleaned_data.iteritems() if value and key.isdigit()]
+                    zones_filtered = Zone.objects.filter(pk__in=ids).all()
+                    self.save_zones_and_send_sms(request,
+                                                 zones_filtered,
+                                                 district,
+                                                 zone_form.cleaned_data["sms"])
 
-                elif key == 'all':
-                    for zone_id in temp:
-                        self.custom_save_zones(zone_id, result["temp_sms"])
-                        task_query_zone.delay(zone_id, result["temp_sms"].sms)
+                messages.success(request, "The messages have been sent")
+                return HttpResponseRedirect(reverse("admin:sms_sendsms_changelist"))
+        else:
+            zone_form = ChooseZonesForm(queryset=zones)
 
+        context = {"title": "Send SMS to Headteachers in Districts",
+                   "opts": opts,
+                   "app_label": opts.app_label,
+                   "zone_form": zone_form }
+        return render(request,
+                      "admin/sms/sendsms/zones_view.html",
+                      context)
 
     def districts_view(self, request):
         """
@@ -228,9 +225,22 @@ class SendSMSAdmin(ManagePermissions):
                 task_query_zone.delay(zone.id, sendsms.sms)
 
 
+    def save_zones_and_send_sms(self, request, zones_qs, district_obj, sms, to_all=False):
+        """This function gets the districts queryset and sms, then Creates a new SendSMS object,
+            then saves it in SMSZones clas, after which sends to the celery worker which sends the sms
+        """
+        sendsms = SendSMS.objects.create(sms=sms,
+                                         district=district_obj,
+                                         user=request.user,
+                                         sent_to_all=to_all)
+        for zone in zones_qs:
+            self.custom_save_zones(zone.id, sendsms)
+            task_query_zone.delay(zone.id, sendsms.sms)
+
+
     def add_view(self, request, form_url='', extra_context=None):
         if self.is_district_admin(request):
-            return super(SendSMSAdmin, self).add_view(request, form_url='', extra_context=None)
+            return HttpResponseRedirect(reverse("admin:sms_sendsms_zones_view"))
 
         elif self.is_rts_staff(request):
             return HttpResponseRedirect(reverse("admin:sms_sendsms_districts_view"))
@@ -245,7 +255,8 @@ class SendSMSAdmin(ManagePermissions):
     def get_urls(self):
         urls = super(SendSMSAdmin, self).get_urls()
         my_urls = patterns('',
-            url(r'/admin/sms/sendsms/sms/districts', self.districts_view, name="sms_sendsms_districts_view"))
+            url(r'/admin/sms/sendsms/sms/districts', self.districts_view, name="sms_sendsms_districts_view"),
+            url(r'/admin/sms/sendsms/sms/zones', self.zones_view, name="sms_sendsms_zones_view"),)
         return my_urls + urls
 
 
