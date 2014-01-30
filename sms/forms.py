82587082c8a23e6ffb053f4ca6d2273455540c22
gitumarkk <gitumarkk@gmail.com>
2014-01-30 18:15:52 +0200

    added dynamic for to form

--- sms/forms.py
+++ sms/forms.py
@@ -21,3 +21,24 @@ class ChooseDistricsForm(forms.Form):
             raise forms.ValidationError(_("Choose all or specific districts not both"))
 
         return cleaned_data
+
+
+class ChooseZonesForm(forms.Form):
+    sms = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}))
+    send_to_all = forms.BooleanField(label=_("All"), required=False)
+
+    def __init__(self, *args, **kwargs):
+        queryset = kwargs.pop("queryset", None)
+        super(ChooseZonesForm, self).__init__(*args, **kwargs)
+        for query in queryset:
+            self.fields["%s" % query.id ] = forms.BooleanField(label=query.name, required=False)
+
+    def clean(self):
+        cleaned_data = super(ChooseZonesForm, self).clean()
+
+        data = dict((k, v) for k, v in cleaned_data.iteritems() if v and k != "sms")
+
+        if "send_to_all" in data and len(data) > 1:
+            raise forms.ValidationError(_("Choose all or specific zones not both"))
+
+        return cleaned_data
