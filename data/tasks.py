10eaee81a2eb21d4ac7cc5dc05ded75e4fce4941
Mike Jones <mike@westerncapelabs.com>
2014-10-20 17:48:38 +0200

    Add API endpoint override from nostream default

--- data/tasks.py
+++ data/tasks.py
@@ -12,10 +12,10 @@ def vumi_fire_metric(metric, value, agg, sender=None):
             sender = HttpApiSender(
                 account_key=settings.ACCOUNT_ID,
                 conversation_key=settings.CONVERSATION_ID,
-                conversation_token=settings.CONVERSATION_TOKEN
+                conversation_token=settings.CONVERSATION_TOKEN, 
+                api_url=settings.API_URL
             )
         sender.fire_metric(metric, value, agg=agg)
         return sender
     except SoftTimeLimitExceeded:
         logger.error('Soft time limit exceed processing metric fire to Vumi HTTP API via Celery', exc_info=True)
-
