7fe503ee1f0ba28bc210baa1b54fece04a94f281
gsvr <george@westerncapelabs.com>
2014-11-11 08:38:32 +0200

    undo dev settings changes

--- rts/settings.py
+++ rts/settings.py
@@ -27,8 +27,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'rts',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost'
+
 
     }
 }
